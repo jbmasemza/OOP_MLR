b999e6f8df4f727e03c0ef66af802373c8e707a2
Recruits <you@example.com>
2020-05-15 17:14:59 +0200

    final code

--- oop.py
+++ oop.py
@@ -1,27 +1,18 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+# ErrorCalculator Class
 class ErrorCalculator:
 
-    def __init__(self, y, y_pred):
+    def __init__(self, y, y_predict):
 
         self.y          =   np.array(y)       
-        self.y_pred     =   np.array(y_pred)  
-
-   
-
-    def dimention(self):
-
-        if len(self.y.shape) == len(self.y_pred.shape):
-            return True
-
-        else:
-            raise ValueError(f'shape of y: {self.y} != shape of y_pred: {self.y_pred}')
-
+        self.y_predict     =   np.array(y_predict)  
 
 
     def get_residuals(self):
 
-        residuals = self.y - self.y_pred
+        residuals = self.y - self.y_predict
         return residuals
 
     def get_standardised_residuals(self):
@@ -31,12 +22,12 @@ class ErrorCalculator:
 
     def get_mse(self):
 
-        mse = np.square(np.subtract(self.y , self.pred)).mean()
+        mse = np.square(np.subtract(self.y, self.y_predict)).mean()
         return mse
 
     def get_rmse(self):
 
-        rmse = np.sqrt(((self.y_pred - self.y)**2).mean())
+        rmse = np.sqrt(((self.y_predict - self.y)**2).mean())
         return rmse
 
     def error_summary(self):
@@ -46,9 +37,41 @@ class ErrorCalculator:
         rmse_max = max(self.rmse)
         mse_min = min(self.mse)
         mse_max = max(self.mse)
-        print(f'standard residual: {std_resid_min}')
-        print(f'standard residual: {std_resid_max}')
-        print(f'min rmse: {rmse_min}')
-        print(f'max rmse: {rmse_max}')
-        print(f'min mse: {mse_min}')
-        print(f'max mse: {mse_max}')
+        return f'standard residual: {std_resid_min}'
+        return f'standard residual: {std_resid_max}' 
+        return f'min rmse: {rmse_min}'
+
+# Plotter
+class Plotter():
+    def __init__(self,y_test,ypred):
+        self.y_test = y_test
+        self.ypred = ypred
+    
+    def run_calculations(self):
+        return self.y_test - self.ypred
+    
+    def plot(self):
+        plt.hist(self.y_test - self.ypred)
+        plt.title('Residuals Plot for predictions')
+        plt.xlabel('Residuals')
+        plt.ylabel('Frequency')
+        return plt.show()
+
+
+# Class histogram plotter
+class HistogramPlotter(Plotter):
+    def __init__(self, y_test,ypred):
+        Plotter.__init__(self, y_test, ypred)
+
+#Class scatter plotter        
+class ScatterPlotter(Plotter):
+    def __init__(self, y_test, ypred):
+        Plotter.__init__(self, y_test, ypred)
+
+    def plot(self):
+        chart = pd.DataFrame({"y_test": self.y_test, "y_prediction": self.ypred})
+        chart.plot.scatter(x="y_test", y="y_prediction", c="DarkBlue")
+        plt.title("Prediction vs Actual values")
+        plt.xlabel("Actual")
+        plt.ylabel("Prediction")
+        return plt.show()
